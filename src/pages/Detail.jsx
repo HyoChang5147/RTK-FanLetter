94a54dde5e76dbcf993bec9332ac06cf58ae85e7
HyoChang5147 <143697280+HyoChang5147@users.noreply.github.com>
2023-11-29 22:31:41 +0900

    redux toolkit 1차 완성

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation, useParams, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { updateLetterContent, removeLetter } from "../redux/modules/letters";
-
+import styled from "styled-components";
 const Detail = () => {
   const { id } = useParams();
   const location = useLocation();
@@ -42,8 +42,10 @@ const Detail = () => {
   };
 
   const handleDelete = () => {
-    if (window.confirm("정말로 삭제하시겠습니까?")) {
+    const confirmDelete = window.confirm("정말로 삭제하시겠습니까?");
+    if (confirmDelete) {
       dispatch(removeLetter(letter.id));
+      window.location.href = "/";
     }
   };
 
@@ -58,29 +60,126 @@ const Detail = () => {
   }
 
   return (
-    <div>
-      <h2>Letter Detail</h2>
-      <p>Nickname: {letter.nickname}</p>
-      {isEditing ? (
-        <>
-          <textarea
+    <Container>
+      <ContentWrapper>
+        <h2>닉네임: {letter.nickname}</h2>
+        {isEditing ? (
+          <TextArea
             value={newContent}
             onChange={(e) => setNewContent(e.target.value)}
-            placeholder="새로운 내용 입력"
+            placeholder="최대 100자 까지 작성할 수 있습니다"
+            maxLength={100}
           />
-          <button onClick={handleUpdateContent}>완료</button>
-          <button onClick={handleCancelEdit}>취소</button>
-        </>
-      ) : (
-        <p>Contents: {letter.contents}</p>
-      )}
-      {!isEditing && <button onClick={() => setIsEditing(true)}>수정</button>}
-      <Link to="/" onClick={handleDelete}>
-        삭제하기
-      </Link>
-      <Link to="/">Home</Link>
-    </div>
+        ) : (
+          <p>내용: {letter.contents}</p>
+        )}
+        <CreatedAt>{letter.createdAt}</CreatedAt>
+      </ContentWrapper>
+      <ButtonContainer>
+        {isEditing ? (
+          <>
+            <Button onClick={handleUpdateContent}>완료</Button>
+            <Button onClick={handleCancelEdit}>취소</Button>
+          </>
+        ) : (
+          <Button onClick={() => setIsEditing(true)}>수정</Button>
+        )}
+        <DeleteLink onClick={handleDelete}>삭제하기</DeleteLink>
+        <HomeButton to="/">Home</HomeButton>
+      </ButtonContainer>
+    </Container>
   );
 };
 
 export default Detail;
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #fe3228;
+  border-radius: 8px;
+
+  &:hover {
+    backdrop-filter: blur(8px);
+  }
+`;
+
+const ContentWrapper = styled.div`
+  margin-bottom: 20px;
+
+  h2 {
+    font-size: 35px;
+    font-weight: bold;
+    margin-bottom: 10px;
+    text-align: center;
+  }
+
+  p {
+    font-size: 30px;
+    margin-bottom: 10px;
+    font-weight: bold;
+    text-align: center;
+  }
+`;
+
+const CreatedAt = styled.div`
+  font-size: 0.8em;
+  text-align: right;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 150px;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+`;
+
+const Button = styled.button`
+  margin-right: 10px;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: #ffcc00;
+  font-size: 14px;
+
+  &:hover {
+    background-color: #ffdb4d;
+  }
+`;
+
+const DeleteLink = styled.div`
+  background-color: #fe5b52;
+  color: #fff;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  font-size: 14px;
+  margin-right: 260px;
+
+  &:hover {
+    background-color: #fe3228;
+  }
+`;
+
+const HomeButton = styled(Link)`
+  background-color: #007bff;
+  color: #fff;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  text-decoration: none;
+  font-size: 14px;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+`;
